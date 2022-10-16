86a1c68fafa82dfc1b70a12c92546fd7713e2533
yuhii167 <68911007+yuhii167@users.noreply.github.com>
2022-10-16 19:54:30 +0700

    make it better

--- blog/models/article_models.py
+++ blog/models/article_models.py
@@ -29,7 +29,7 @@ class Article(models.Model):
     # BLOG MODEL FIELDS
     category = models.ForeignKey(Category, on_delete=models.CASCADE,
                                  related_name='articles')
-    title = models.CharField(max_length=250, null=False, blank=False)
+    title = models.CharField(max_length=200, null=False, blank=False)
     slug = models.SlugField()
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='articles')
