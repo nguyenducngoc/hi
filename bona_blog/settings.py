e17acb191ee6451d607467b9fc93fa12436eb08a
yuhii167 <68911007+yuhii167@users.noreply.github.com>
2022-10-19 14:34:45 +0700

    a

--- bona_blog/settings.py
+++ bona_blog/settings.py
@@ -193,12 +193,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Email Settings (Production)
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = "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"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.sendgrid.net'
+#EMAIL_HOST_USER = 'apikey'
+#EMAIL_HOST_PASSWORD = "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"
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
 # CKEditor Settings
